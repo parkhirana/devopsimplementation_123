83d036b662094dbfd0cc1207f09f4dbe315d4eaa
parkhirana <parkhirana02@gmail.com>
2025-08-26 10:09:06 +0200

    added backend to the sys path for pytest to be able to find app.py

--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -1,3 +1,9 @@
+import sys, os
+import pytest
+
+# Add backend/ to PYTHONPATH
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 import pytest
 from app import app
 
